ddaaa5391e9440adf79b71182966f6a0a5104ecc
Swarnava Basu <basuswarnava2@gmail.com>
2020-08-06 01:02:45 +0530

    working on

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" className="w-full" />
+      <div className="px-6 py-4">
+      <div className="font-bold text-purple-500 text-xl mb-2">
+    Photo by John doe
+      </div>
+      </div>
+     
+   </div>
+  );
+}
+
+export default App;
